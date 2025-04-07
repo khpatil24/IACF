7cfeaafbce27e5eeda92ec3d38db65b33730b44e
kajalp713 <kp177096@gmail.com>
2025-04-07 12:27:39 +0530

    Footer

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
+import ActionSection from './components/ActionSection/ActionSection'
+import HeroBanner from './components/Hero/HeroBanner'
+import Navbar from './components/Navigation/Navbar'
+import WhyHelpSection from './components/WhyHelp/WhyHelpSection'
+import Footer from './components/Footer/Footer'
 
 export default function App() {
   return (
-    <div className="min-h-screen bg-gray-50">
-      <h1 className="bg-red-500 text-red p-4">Test Tailwind</h1>
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <HeroBanner />
+      <ActionSection />
+      <WhyHelpSection />
+      <Footer />
     </div>
   )
 }
